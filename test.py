b2315f6e15940e7dbf2161f013d4d4c70ceaff46
desm <desm80@yandex.ru>
2022-05-17 16:03:19 +0300

    office 17/05

---
+++ test.py
@@ -0,0 +1,36 @@
+class Contact:
+    name: str
+    year_birth: int
+    is_programmer: bool
+
+    def __init__(self,
+                 name: str = 'Denis',
+                 year_birth: int = 1980,
+                 is_programmer: bool = False):
+        self.name = name
+        self.year_birth = year_birth
+        self.is_programmer = is_programmer
+
+    def age_define(self) -> str:
+        if 1946 < self.year_birth < 1980:
+            return 'Олдскул'
+        if self.year_birth >= 1980:
+            return 'Молодой'
+        return 'Старейшина'
+
+    def programmer_define(self) -> str:
+        if self.is_programmer:
+            return 'Программист'
+        return 'Нормальный'
+
+    def show_contact(self) -> str:
+        return (f'{self.name}, '
+                f'категория: {self.age_define()}, '
+                f'статус: {self.programmer_define()}')
+
+    def print_contact(self):
+        print(self.show_contact())
+
+
+denis = Contact('Denis', 1980, False)
+denis.print_contact()
